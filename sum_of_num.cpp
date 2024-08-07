f98f93aa825e7a559cabcf8e393a9e8344801a36
Aashi-sharma30 <114644659+Aashi-sharma30@users.noreply.github.com>
2024-08-07 16:23:39 +0530

    Add files via upload

---
+++ sum_of_num.cpp
@@ -0,0 +1,14 @@
+//wap to print sum oftwo num using pointers
+#include<iostream>
+using namespace std;
+int main(){
+    int x=6;
+    int y=5;
+    int* p1=&x;
+    int*p2=&y;
+
+    cout<<*p1+*p2; // copy se samjho
+
+    
+
+}
